bd67cfc24b880ad7a2badaa1b35e29c39d8b088d
biancaoprisan <biancaoprisan4@gmail.com>
2021-04-08 16:16:00 +0300

    initial commit

---
+++ Seminar7/src/ro/ase/cts/adapter/program/Program.java
@@ -0,0 +1,23 @@
+package ro.ase.cts.adapter.program;
+
+import ro.ase.cts.adapter.clase.AdaptorCreditObiecte;
+import ro.ase.cts.adapter.clase.CreditAbstract;
+import ro.ase.cts.adapter.clase.Leasing;
+
+public class Program {
+
+	public static void afiseazaInformatiiCredit(CreditAbstract credit, float suma) {
+		credit.oferaCredit(suma);
+	}
+	
+	public static void main(String[] args) {
+  
+		Leasing leasing = new Leasing(100, "Ionut");
+		//afiseazaInformatiiCredit(leasing, leasing1.getSuma()); nu putem accesa leasing
+		AdaptorCreditObiecte adaptor= new AdaptorCreditObiecte(leasing);
+		afiseazaInformatiiCredit(adaptor, leasing.getSuma());
+
+
+	}
+
+}
---
+++ Seminar7/src/ro/ase/cts/adapterclase/program/Program.java
@@ -0,0 +1,19 @@
+package ro.ase.cts.adapterclase.program;
+
+import ro.ase.cts.adapterclase.clase.AdaptorCreditClase;
+import ro.ase.cts.adapterclase.clase.CreditAbstract;
+
+public class Program {
+
+	
+	public static void afiseazaInformatiiCredit(CreditAbstract credit) {
+		credit.oferaCredit();
+	}
+	public static void main(String[] args) {
+		
+		AdaptorCreditClase adaptorClase = new AdaptorCreditClase(50, "Ana");
+		afiseazaInformatiiCredit(adaptorClase);
+
+	}
+
+}
